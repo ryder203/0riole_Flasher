446d0be2283174cdb77f406862107b74a6a6e3ef
t-ryder <t-ryder@mailbox.org>
2021-12-22 05:11:34 +0100

    Update to v3
    Prevent softbrick by missing image files | Confirm cleanup in platform-tools folder

--- flasher.bat
+++ flasher.bat
@@ -1,11 +1,17 @@
 @ECHO OFF
 PATH=%PATH%;"%SYSTEMROOT%\System32"
-echo 0riole-Flasher starts...
+echo 0riole-Flasher v3 starts...
 echo Renaming files...
-ren bootloader*.img bootloader.img
-ren radio*.img radio.img
-ren image*.zip image.zip
-ren magisk*.img magiskboot.img
+if exist bootloader*.img ren bootloader*.img bootloader.img
+if exist radio*.img ren radio*.img radio.img
+if exist image*.zip ren image*.zip image.zip
+if exist magisk*.img ren magisk*.img magiskboot.img
+if exist "bootloader.img" if exist "radio.img"  if exist "image.zip" if exist "magiskboot.img" goto :startflash
+if not exist "bootloader.img" goto :bootloaderfilemissing
+if not exist "radio.img" goto :radiofilemissing
+if not exist "image.zip" goto :imagefilemissing
+if not exist "magiskboot.img" goto :magiskbootfilemissing
+:startflash
 echo Let's start flashing...
 ping -n 5 127.0.0.1 >nul
 adb reboot bootloader
@@ -17,29 +23,56 @@ ping -n 5 127.0.0.1 >nul
 fastboot flash radio radio.img --slot all
 fastboot reboot bootloader
 ping -n 5 127.0.0.1 >nul
-fastboot update image.zip
-ping -n 5 127.0.0.1 >nul
-echo Please wait...
-ping -n 30 127.0.0.1 >nul
-adb kill-server
-ping -n 5 127.0.0.1 >nul
-echo Please wait...
-adb start-server
+fastboot update --skip-reboot image.zip
 ping -n 5 127.0.0.1 >nul
 echo Please wait...
 ping -n 5 127.0.0.1 >nul
-adb reboot bootloader
+fastboot reboot bootloader
 ping -n 5 127.0.0.1 >nul
 fastboot flash boot magiskboot.img --slot all
 ping -n 5 127.0.0.1 >nul
 fastboot reboot
+:choice
+set /P c=Do you want to clean up and delete the update files [Y/N]?
+if /I "%c%" EQU "Y" goto :cleanupnow
+if /I "%c%" EQU "N" goto :quit
+goto :choice
+:cleanupnow
+del /f bootloader.img radio.img image.zip magiskboot.img
 echo Cleaning up platform-tools folder...
-del bootloader.img
-del radio.img
-del image.zip
-del magiskboot.img
 ping -n 5 127.0.0.1 >nul
 echo All done...
+echo Press any key to exit...
+pause >nul
+exit
+:quit
+echo Not cleaning up platform-tools folder...
+ping -n 5 127.0.0.1 >nul
+echo All done...
+echo Press any key to exit...
+pause >nul
+exit
+:bootloaderfilemissing
+ping -n 3 127.0.0.1 >nul
+echo Bootloader file missing...
+echo Press any key to exit...
+pause >nul
+exit
+:radiofilemissing
+ping -n 3 127.0.0.1 >nul
+echo Radio file missing...
+echo Press any key to exit...
+pause >nul
+exit
+:imagefilemissing
+ping -n 3 127.0.0.1 >nul
+echo image.zip file missing...
+echo Press any key to exit...
+pause >nul
+exit
+:magiskbootfilemissing
+ping -n 3 127.0.0.1 >nul
+echo Magisk boot.img file missing...
 echo Press any key to exit...
 pause >nul
 exit
